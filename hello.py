dd2de450e8da564e0fbc6c7aea228343d50753ef
amanda7144 <137000864+amanda7144@users.noreply.github.com>
2023-06-26 15:09:38 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 print("Hello World")
 print(\t This is my first Git repository on GitHub")
+
+def print_messege(name='Python'):
+    print("Welcome to the class from {name}!)
+
+  name = "Julia"
+  print_message (name)
